efde981036aaba8cda85447d21b3272ecd1cf054
Atuldubey98 <atul.dubey@algoscale.com>
2024-02-05 14:10:03 +0530

    RAG-291 feature : added wsHost in params

--- assets/bot-iframe.js
+++ assets/bot-iframe.js
@@ -4,11 +4,22 @@ const model = {
 const view = {
   init: function () {
     const widgetId = this.getWidgetId();
+    const wsHost = this.getWsHost();
+    this.setWsHost(wsHost);
     this.setWidgetIdForWidget(widgetId);
     this.setThemeForWidget();
   },
+  setWsHost: function (wsHost) {
+    const widget = document.getElementById("chat-widget");
+    if (wsHost) {
+      widget.dataset.wsHost = wsHost;
+    }
+  },
   getWidgetId: function () {
     return this.getParams("widgetId");
+  },
+  getWsHost: function () {
+    return this.getParams("wsHost");
   },
   setWidgetIdForWidget: function (widgetId) {
     const widget = document.getElementById("chat-widget");
