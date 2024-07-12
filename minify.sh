2db98b925fb0e7f2822f599f4b564b9dfe0aad75
Atuldubey98 <atul.dubey@algoscale.com>
2024-07-12 12:24:35 +0530

    fix : bot iframe and call iframe min css js

--- minify.sh
+++ minify.sh
@@ -1,3 +1,5 @@
 npm i uglify-js clean-css -g
 uglifyjs -o ./assets/insighto.min.js ./assets/insighto.js
+uglifyjs -o ./assets/bot-iframe.min.js ./assets/bot-iframe.js
+uglifyjs -o ./call_assets/call_iframe.min.js ./call_assets/call_iframe.js
 cleancss -o ./assets/insighto.min.css ./assets/insighto.css
