a0089dd71b4abe514d3232f0029ea9dff97596b0
Atuldubey98 <atul.dubey@algoscale.com>
2024-08-23 15:39:56 +0530

    feature : voice widget

--- minify.sh
+++ minify.sh
@@ -2,4 +2,5 @@ npm i uglify-js clean-css -g
 uglifyjs -o ./assets/insighto.min.js ./assets/insighto.js
 uglifyjs -o ./assets/bot-iframe.min.js ./assets/bot-iframe.js
 uglifyjs -o ./call_assets/call_iframe.min.js ./call_assets/call_iframe.js
+uglifyjs -o ./web_call_assets/web_call_iframe.min.js ./web_call_assets/web_call_iframe.js
 cleancss -o ./assets/insighto.min.css ./assets/insighto.css
