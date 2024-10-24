c6202cee987260eacdcb98a934c26a748c01fd25
Atul Dubey <atul.dubey@algoscale.com>
2024-10-24 18:29:59 +0530

    fix : phone icon

--- web_call_assets/web_call_iframe.js
+++ web_call_assets/web_call_iframe.js
@@ -2,14 +2,23 @@ const view = {
   init: function () {
     const widgetId = this.getWidgetId();
     const wsHost = this.getWsHost();
+    const deployType = this.getDeployType();
+    this.setDeployType(deployType);
     this.setWidgetIdForWidget(widgetId);
     this.setWsHost(wsHost);
   },
   getWidgetId: function () {
     return this.getParams("widgetId");
   },
+  getDeployType: function () {
+    return this.getParams("deployType");
+  },
+  setDeployType: function (deployType) {
+    const widget = document.getElementById("webCallWidget");
+    if (deployType) widget.dataset.deployType = deployType;
+  },
   setWsHost: function (wsHost) {
-    const widget = document.getElementById("webCallWidget");    
+    const widget = document.getElementById("webCallWidget");
     if (wsHost) widget.dataset.wsHost = wsHost;
   },
   getWsHost: function () {
